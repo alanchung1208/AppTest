e52bcce7af370bba60f479b7d3dc1578e1b8f49a
alan <alan.december@gmail.com>
2019-05-25 10:45:07 +0800

    change 456

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            Console.Write("app test 123....");
+            Console.Write("app test 123 456....");
         }
     }
 }
